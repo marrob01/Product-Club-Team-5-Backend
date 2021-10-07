cd9099a3546b1b197c2aeefaaf4868f053987e58
Sebastian G <gayle054@gmail.com>
2021-10-06 18:06:33 -0700

    changes made

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ let connectionString = "";
 if (process.env.NODE_ENV === "production") {
     connectionString = process.env.DB_URL
 } else {
-    connectionString = "mongodb://localhost/skills";
+    connectionString = "mongodb://localhost/covidbudget";
 }
 
 mongoose.connect(connectionString, mongooseConnectionConfig);
