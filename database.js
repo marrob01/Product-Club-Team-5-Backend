ebe3c79f33d302c9a2f9bb404170f13c65d52fbd
stacy leitstein <unique523@yahoo.com>
2021-10-06 21:18:06 -0400

    updated data.json to match schemas

--- database.js
+++ database.js
@@ -8,7 +8,8 @@ let connectionString = "";
 if (process.env.NODE_ENV === "production") {
     connectionString = process.env.DB_URL
 } else {
-    connectionString = "mongodb://localhost/skills";
-}
+    connectionString = "mongodb://localhost/covidBudget";
+    
+} 
 
 mongoose.connect(connectionString, mongooseConnectionConfig);
