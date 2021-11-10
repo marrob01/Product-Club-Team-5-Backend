35c70f425dceb233c4ad8b18712fff45ddc8b3fa
stacy leitstein <unique523@yahoo.com>
2021-11-10 10:40:18 -0400

    trying to deploy with netlify, installed severless-http and netlify-cli

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let stateRouter = require("./state/router");
 let federalRouter = require("./federal/router");
+require('serverless-http');
 
 let router = new express.Router();
 router.use('/state', stateRouter);
